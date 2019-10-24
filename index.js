35ebc20790fc67bb133ea62dcc44f9582826a8df
Hacene <hacenehcn97@gmail.com>
2019-10-24 21:06:55 +0200

    setup .prettierrc

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const slugify = require('slugify');
 const replaceTemplate = require('./modules/replaceTemplate');
 
 ////////////////////////////////////////////////////////////////
-// FICHIER 
+// FICHIER
 
 // const slugify = require('slugify');
 // const replaceTemplate = require('./modules/replaceTemplate');
@@ -38,49 +38,61 @@ const replaceTemplate = require('./modules/replaceTemplate');
 
 //
 ////////////////////////////////////////////////////////////////
-//SERVER 
+//SERVER
 
 //de maniére synchrone
-    const tempOverview = fs.readFileSync(`${__dirname}/templates/template-overview.html`,'utf-8');
-    const tempCard = fs.readFileSync(`${__dirname}/templates/template-card.html`,'utf-8');
-    const tempProduct = fs.readFileSync(`${__dirname}/templates/template-product.html`,'utf-8');
-  
-  const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, 'utf-8');
-  const dataObj = JSON.parse(data); //JSON vers tableau
+const tempOverview = fs.readFileSync(`${__dirname}/templates/template-overview.html`, 'utf-8');
+const tempCard = fs.readFileSync(`${__dirname}/templates/template-card.html`, 'utf-8');
+const tempProduct = fs.readFileSync(`${__dirname}/templates/template-product.html`, 'utf-8');
 
-  const slugs = dataObj.map( el => slugify(el.productName,{lower : true}));
-  
-  const server = http.createServer((req, res) => {
-    const { query, pathname } = url.parse(req.url, true);//syntaxe de ES6 url.parse(req.url, true); retourne un ensemble de variables qui contient query et pathname
-  
-    // Overview page
-    if (pathname === '/' || pathname === '/overview') {
-      res.writeHead(200, {'Content-type': 'text/html'});
-      // map permet de boucler sur le tableau 
-      const cardsHtml = dataObj.map(el => replaceTemplate(tempCard, el)).join('');//join est une fonction qui retourne un String a partir du tableau
-      const output = tempOverview.replace('{%PRODUCT_CARDS%}',cardsHtml);
-      res.end(output);
-  
-      // Product page
-    } else if (pathname === '/product') {
-      res.writeHead(200, {'Content-type': 'text/html'});
-      const product = dataObj[query.id];// quel produit afficher 
-      const output = replaceTemplate(tempProduct, product);
-      res.end(output);
-  
-      // API
-    } else if (pathname === '/api') {
-      res.writeHead(200, {'Content-type': 'application/json'});
-      res.end(data);
-  
-      // Not found
-    } else {
-      res.writeHead(404, {'Content-type': 'text/html','my-own-header': 'hello-world'});
-      res.end('<h1>Page not found!</h1>');
-    }
-  });
-  
-  server.listen(8000, '127.0.0.1', () => {
-    console.log('Listening to requests on port 8000');
-  });
-  
+const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, 'utf-8');
+const dataObj = JSON.parse(data); //JSON vers tableau
+
+const slugs = dataObj.map(el =>
+  slugify(el.productName, {
+    lower: true
+  })
+);
+
+const server = http.createServer((req, res) => {
+  const { query, pathname } = url.parse(req.url, true); //syntaxe de ES6 url.parse(req.url, true); retourne un ensemble de variables qui contient query et pathname
+
+  // Overview page
+  if (pathname === '/' || pathname === '/overview') {
+    res.writeHead(200, {
+      'Content-type': 'text/html'
+    });
+    // map permet de boucler sur le tableau
+    const cardsHtml = dataObj.map(el => replaceTemplate(tempCard, el)).join(''); //join est une fonction qui retourne un String a partir du tableau
+    const output = tempOverview.replace('{%PRODUCT_CARDS%}', cardsHtml);
+    res.end(output);
+
+    // Product page
+  } else if (pathname === '/product') {
+    res.writeHead(200, {
+      'Content-type': 'text/html'
+    });
+    const product = dataObj[query.id]; // quel produit afficher
+    const output = replaceTemplate(tempProduct, product);
+    res.end(output);
+
+    // API
+  } else if (pathname === '/api') {
+    res.writeHead(200, {
+      'Content-type': 'application/json'
+    });
+    res.end(data);
+
+    // Not found
+  } else {
+    res.writeHead(404, {
+      'Content-type': 'text/html',
+      'my-own-header': 'hello-world'
+    });
+    res.end('<h1>Page not found!</h1>');
+  }
+});
+
+server.listen(8000, '127.0.0.1', () => {
+  console.log('Listening to requests on port 8000');
+});
